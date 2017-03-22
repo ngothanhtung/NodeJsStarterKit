cfa1900a0217d7649843dc6a2c6d26013e861b64
Ngo Thanh Tung <tungnt@softech.vn>
2017-03-22 14:14:55 +0700

    Update Mongodb

--- routes/mongodb.js
+++ routes/mongodb.js
@@ -1,74 +1,6 @@
 var express = require('express');
 var router = express.Router();
 
-<<<<<<< HEAD
-var MongoClient = require('mongodb').MongoClient;
-
-/* GET home page. */
-var products = [{
-    id: 1,
-    name: 'iphone 7',
-    price: 900,
-    imageUrl: 'http://cdn.shopify.com/s/files/1/0742/5113/products/black_black_aa9cea5f-8054-4a54-b594-c1066ef371a9_grande.png?v=1473979599',
-}, {
-    id: 2,
-    name: 'iphone 7 plus',
-    price: 980,
-    imageUrl: 'http://cdn.shopify.com/s/files/1/0742/5113/products/black_black_aa9cea5f-8054-4a54-b594-c1066ef371a9_grande.png?v=1473979599',
-}, {
-    id: 3,
-    name: 'iphone 7 plus',
-    price: 980,
-    imageUrl: 'http://cdn.shopify.com/s/files/1/0742/5113/products/black_black_aa9cea5f-8054-4a54-b594-c1066ef371a9_grande.png?v=1473979599',
-}, {
-    id: 4,
-    name: 'iphone 7 plus',
-    price: 980,
-    imageUrl: 'http://cdn.shopify.com/s/files/1/0742/5113/products/black_black_aa9cea5f-8054-4a54-b594-c1066ef371a9_grande.png?v=1473979599',
-}, {
-    id: 5,
-    name: 'iphone 7 plus',
-    price: 980,
-    imageUrl: 'http://cdn.shopify.com/s/files/1/0742/5113/products/black_black_aa9cea5f-8054-4a54-b594-c1066ef371a9_grande.png?v=1473979599',
-}, {
-    id: 6,
-    name: 'iphone 7 plus',
-    price: 980,
-    imageUrl: 'http://cdn.shopify.com/s/files/1/0742/5113/products/black_black_aa9cea5f-8054-4a54-b594-c1066ef371a9_grande.png?v=1473979599',
-}];
-
-router.get('/', function (req, res, next) {
-    var mongodb_url = 'mongodb://188.166.218.3:27017/OnlineShop';
-    MongoClient.connect(mongodb_url, function (err, db) {
-        if (err) console.log(err);
-        else {
-
-            findDocuments(db, function (result) {
-                var dataModel = {
-                    data: result
-                };
-                db.close();
-                res.render('index', dataModel);
-            });
-        }
-    });
-
-    // var dataModel = {
-    //     data: products
-    // };
-    // res.render('index', dataModel);
-
-});
-
-var findDocuments = function (db, callback) {
-    var collection = db.collection('products');
-    collection.find().toArray(function (err, items) {
-        return callback(items);
-    });
-};
-
-module.exports = router;
-=======
 var MongoClient = require('mongodb').MongoClient
 var assert = require('assert');
 
@@ -144,4 +76,3 @@ var removeDocument = function (db, callback) {
 };
 
 module.exports = router;
->>>>>>> origin/master
