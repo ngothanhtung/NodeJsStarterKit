38318c66ad78fbe012d8381f9ba8e726f66f5fc7
Ngo Thanh Tung <tung.ngo@outlook.com>
2017-04-11 23:26:51 +0700

    Update MongoDB

--- routes/mongodb.js
+++ routes/mongodb.js
@@ -1,101 +1,53 @@
 var express = require('express');
 var router = express.Router();
-// 1. Khai bao thu vien MONGODB
-var MongoClient = require('mongodb').MongoClient;
+var db = require('../helpers/MongoDbHelper');
 
-
-// 2. Connection URL
-var url = 'mongodb://localhost:27017/TestDB';
-
-// 3. INSERT
-var insertDocument = function (db, data, callback) {
-    // Get the documents collection
-    var collection = db.collection('products');
-    // Insert some documents
-    collection.insertOne(data, function (err, result) {
-        callback(result);
+// TEST: INSERT
+router.get('/insert', function (req, res, next) {
+    var product = {
+        name: 'iPhone 7 Plus 128GB',
+        price: 900,
+        discount: 0
+    };
+    db.insertDocument(product, 'products', function (result) {
+        res.json(result);
     });
-};
-// 4. FIND (GET ALL)
-var findDocuments = function (db, condition, callback) {
-    // Get the documents collection
-    var collection = db.collection('products');
-    // Find some documents
-    collection.find(condition).toArray(function (err, result) {
-        callback(result);
-    });
-};
-
-
-var updateDocument = function (db, callback) {
-    // Get the documents collection
-    var collection = db.collection('documents');
-    // Update document where a is 2, set b equal to 1
-    collection.updateOne({a: 2}
-        , {$set: {b: 1}}, function (err, result) {
-            console.log("Updated the document with the field a equal to 2");
-            callback(result);
-        });
-};
-
-var removeDocument = function (db, callback) {
-    // Get the documents collection
-    var collection = db.collection('documents');
-    // Delete document where a is 3
-    collection.deleteMany({discount: '0'}, function (err, result) {
-        console.log("Removed the document with the field a equal to 3");
-        callback(result);
-    });
-};
-
-
-/* GET home page. */
-
-router.get('/create', function (req, res, next) {
-    res.render('createmongo');
 });
 
-router.post('/create', function (req, res, next) {
-
-    // 3.1 TEST INSERT
-    // Use connect method to connect to the server
-    MongoClient.connect(url, function (err, db) {
-        console.log("Connected successfully to server");
-        var product = req.body;
-        insertDocument(db, product, function (result) {
-            console.log(result);
-            db.close();
-        });
+// TEST: UPDATE
+router.get('/update', function (req, res, next) {
+    var id = '58ed0329dc0d3e5eba5f5540';
+    var data = {
+        discount: 10
+    };
+    db.updateDocument(id, data, 'products', function (result) {
+        res.json(result);
     });
-
-    res.render('createmongo');
 });
 
-router.get('/getAll', function (req, res, next) {
-    // 4.1 TEST GET ALL
-    // Use connect method to connect to the server
-    MongoClient.connect(url, function (err, db) {
-        console.log("Connected successfully to server");
-        findDocuments(db, {}, function (result) {
-            console.log(result);
-            db.close();
-            // CHUYEN DATA MODEL SANG CLIENT
-            res.render('viewmongo', {data: result});
-        });
+// TEST: DELETE
+router.get('/delete', function (req, res, next) {
+    var id = '58ed0329dc0d3e5eba5f5540';
+
+    db.removeDocument(id, 'products', function (result) {
+        res.json(result);
     });
-    //res.send('ok');
 });
 
+// TEST: FIND ONE
+router.get('/getOne', function (req, res, next) {
+    var id = '58ed0329dc0d3e5eba5f5540';
 
-router.get('/', function (req, res, next) {
-    // Use connect method to connect to the server
-    MongoClient.connect(url, function (err, db) {
-        console.log("Connected successfully to server");
-        db.close();
+    db.findDocument(id, 'products', function (result) {
+        res.json(result);
     });
-
-    res.send('ok');
 });
 
+// TEST: FIND MANY
+router.get('/getMany', function (req, res, next) {
+    db.findDocuments({}, 'products', function (result) {
+        res.json(result);
+    });
+});
 
 module.exports = router;
