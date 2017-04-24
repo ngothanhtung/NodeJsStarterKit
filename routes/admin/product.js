71d75448a3e1817565baf40b79cc2033636cbac1
Ngo Thanh Tung <tungnt@softech.vn>
2017-04-24 23:14:38 +0700

    update admin

--- routes/admin/product.js
+++ routes/admin/product.js
@@ -1,16 +1,13 @@
 var express = require('express');
 var router = express.Router();
-var demo = require('../../demo.js');
-/* GET home page. */
+var db = require('../../helpers/MongoDbHelper');
+
 router.get('/', function (req, res, next) {
-
-    var model = {
-        data: demo.products()
-    };
-    var view = 'admin/product/index';
-    res.render(view, model);
+    db.findDocuments({}, 'products', function (result) {
+        var view = 'admin/product/index';
+        res.render(view, {model: result});
+    });
 });
-
 
 router.get('/create', function (req, res, next) {
     var view = 'admin/product/create';
@@ -18,12 +15,27 @@ router.get('/create', function (req, res, next) {
 });
 
 router.post('/create', function (req, res, next) {
-    console.log(req.body);
-    var model = {
-        data: demo.products()
-    };
-    var view = 'admin/product/index';
-    res.render(view, model);
+    var data = req.body;
+    db.insertDocument(data, 'products', function (result) {
+        var url = '/admin/product';
+        res.redirect(url);
+    });
 });
 
+router.get('/edit/:id', function (req, res, next) {
+    var id = req.params.id;
+    db.findDocument(id, 'products', function (result) {
+        var view = 'admin/product/edit';
+        res.render(view, {model: result});
+    });
+});
+
+router.post('/edit/:id', function (req, res, next) {
+    var id = req.params.id;
+    var data = req.body;
+    db.updateDocument(id, data, 'products', function (result) {
+        var url = '/admin/product';
+        res.redirect(url);
+    });
+});
 module.exports = router;
